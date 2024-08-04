685e3d1d0bb4413da1e9879932fbe9cc14e876e7
Patryk Mielech <patryk.mielech@protonmail.com>
2024-08-04 17:37:48 +0200

    Created the project
    Set up the project

---
+++ RequestService/Program.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using RequestService.Models;
+
+namespace RequestService;
+
+class Program
+{
+  public static async Task Main(string[] args)
+  {
+
+    BeActiveParser gym = new BeActiveParser();
+    await gym.GetValue();
+    
+  }
+}
